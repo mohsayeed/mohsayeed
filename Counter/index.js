a729883272fc253c63310e0c099e9433907d7e60
Shaik Mohammed Sayeed <74594722+mohsayeed@users.noreply.github.com>
2021-05-22 08:40:04 +0530

    Update index.js

--- Counter/index.js
+++ Counter/index.js
@@ -1,11 +1,18 @@
-let countel=document.getElementById("elemnt-no")
-console.log(countel)
-let count =0;
-function incr(){
-    count=count+1
-    countel.innerText=count;
+let countel = document.getElementById("elemnt-no");
+let savel = document.getElementById("elemt-save");
+console.log(countel);
+let count = 0;
+let s = "";
+function incr() {
+  count = count + 1;
+  countel.innerText = count;
 }
-function reset(){
-    countel.innerText=0;
-    count=0;
+function reset() {
+  countel.innerText = 0;
+  count = 0;
+  savel.innerHTML = "";
+}
+function save() {
+  let s = countel.innerText;
+  savel.innerHTML += s + "-";
 }
